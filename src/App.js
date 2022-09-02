930db81c0123e2962ccffbf3e2295e9ad615d477
iretoms <iretomiwajonathan@gmail.com>
2022-09-01 21:52:29 -0700

    initial pages and routes setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {Routes, Route} from "react-router-dom"
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
+
 function App() {
   return (
     <>
-    <h1>heyyy</h1>
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
     </>
   );
 }
