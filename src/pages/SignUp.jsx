00289d6668a9af72aea5e6e87af9ba1e76552acf
iretoms <iretomiwajonathan@gmail.com>
2022-09-02 17:50:56 -0700

    sign up, adding user to firestore database

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -20,6 +23,31 @@ const SignUp = () => {
     }));
   };
 
+  const onSubmit = async(e) =>{
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -27,7 +55,7 @@ const SignUp = () => {
           <p className="pageHeader">Welcome Back</p>
         </header>
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="text"
               id="name"
