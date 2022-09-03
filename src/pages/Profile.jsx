7894de278b7fae94b3729c164fe9701a20a8063e
iretoms <iretomiwajonathan@gmail.com>
2022-09-02 22:44:14 -0700

    signing in to firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+  return user ? <h2>Welcome {user.displayName}</h2> : <h2>not logged in</h2>;
 };
 
 export default Profile;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
-import { db } from '../firebase.config'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, SetFormData] = useState({
-    name:"",
+    name: "",
     email: "",
     password: "",
   });
@@ -23,30 +27,34 @@ const SignUp = () => {
     }));
   };
 
-  const onSubmit = async(e) =>{
-    e.preventDefault()
+  const onSubmit = async (e) => {
+    e.preventDefault();
     try {
-      const auth = getAuth()
+      const auth = getAuth();
 
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
-      const user = userCredential.user
+      const user = userCredential.user;
 
       updateProfile(auth.currentUser, {
-        displayName: name
-      })
+        displayName: name,
+      });
 
-      const formDataCopy = {...formData}
-      delete formDataCopy.password
-      formDataCopy.timestamp = serverTimestamp()
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
 
       await setDoc(doc(db, "users", user.uid), formDataCopy);
 
-      navigate('/')
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <>
